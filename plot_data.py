f90910d737e849a783000e261eb5cddcd8c709b3
Andrew Straw <strawman@astraw.com>
2012-07-25 10:39:30 +0200

    isolate time modeling code

--- plot_data.py
+++ plot_data.py
@@ -1,29 +1,7 @@
 import matplotlib.pyplot as plt
 from matplotlib.mlab import csv2rec
 import numpy as np
-
-def model_remote_to_local(remote_timestamps, local_timestamps, debug=False):
-    """for timestamps"""
-    a1=remote_timestamps[:,np.newaxis]
-    a2=np.ones( (len(remote_timestamps),1))
-    A = np.hstack(( a1,a2))
-    b = local_timestamps[:,np.newaxis]
-    x,resids,rank,s = np.linalg.lstsq(A,b)
-    if debug:
-        print 'in model_remote_to_local: N=%d, resids=%s'%(
-            len(remote_timestamps),resids)
-    gain = x[0,0]
-    offset = x[1,0]
-    return gain,offset
-
-class TimeModel:
-    def __init__(self,gain,offset):
-        self.gain = gain
-        self.offset = offset
-    def timestamp2framestamp(self, mainbain_timestamp ):
-        return (mainbain_timestamp-self.offset)/self.gain
-    def framestamp2timestamp(self, framestamp ):
-        return framestamp*self.gain + self.offset
+from time_model import get_time_model
 
 def persist( xo, yo ):
     x = []
@@ -51,11 +29,8 @@ if 1:
         rec = csv2rec( fname, names=cols)
         data[table] = rec
 
-
-    # fit linear model of relationship mainbrain timestamp and usb trigger_device framestamp
-    gain, offset = model_remote_to_local( data['times']['time_ino'], data['times']['time_host'])
-    time_model = TimeModel(gain, offset)
-
+    time_model = get_time_model(data['times']['time_ino'],
+                                data['times']['time_host'])
     fig = plt.figure()
 
     ax1=fig.add_subplot(2,1,1)
