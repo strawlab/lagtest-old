aabd3d24ff056222e7ff06a8fcf277c9e1c66d67
Andrew Straw <strawman@astraw.com>
2012-07-25 14:04:30 +0200

    towards automated latency finding

--- plot_data.py
+++ plot_data.py
@@ -56,4 +56,51 @@ if 1:
               'b.' )
     ax2.set_xlabel('time (msec)')
 
+    if 1:
+
+        WIDTH = 100
+        accum = {0:[], 1:[]}
+        for switch_row in data['switches']:
+            time_host, value = switch_row
+            time_arduino = time_model.timestamp2framestamp(time_host)
+            
+            idx = np.argmin(abs(data['adcs']['time_ino']-time_arduino))
+            idx_start = idx-WIDTH
+            idx_stop = idx+WIDTH
+            if idx_start < 0:
+                continue
+            if idx_stop >= len(data['adcs']):
+                continue
+            accum[ value ].append( data['adcs']['adc'][idx_start:idx_stop] )
+
+            t = time_model.framestamp2timestamp(
+                data['adcs']['time_ino'][idx_start:idx_stop])
+        t = (t-t[WIDTH])*1000.0
+        m0 = np.mean( accum[0], axis=0 )
+        m1 = np.mean( accum[1], axis=0 )
+
+        s0 = np.std( accum[0], axis=0 )
+        s1 = np.std( accum[1], axis=0 )
+
+        fig = plt.figure()
+        ax1 = fig.add_subplot(2,1,1)
+        # for samples in accum[0]:
+        #     ax1.plot(t,samples,'-', color=(0.7,0.7,0.7))
+        ax1.plot( t, m0, 'b-', lw=2 )
+        ax1.fill_between( t, m0-s0, m0+s0, 
+                          facecolor='b',
+                          alpha=0.4,
+                          linewidth=0 )
+
+        ax2 = fig.add_subplot(2,1,2,sharex=ax1)
+        # for samples in accum[1]:
+        #     ax2.plot(t,samples,'-', color=(0.7,0.7,0.7))
+        ax2.plot( t, m1, 'b-', lw=2 )
+        ax2.fill_between( t, m1-s1, m1+s1, 
+                          facecolor='b',
+                          alpha=0.4,
+                          linewidth=0 )
+
+        ax2.set_xlabel( 'time (msec)')
+
     plt.show()
