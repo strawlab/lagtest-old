8ab113fd2738bb9fa158058f040c1a153abce7a1
Andrew Straw <strawman@astraw.com>
2012-10-23 13:30:54 +0200

    app: do less drawing work in matplotlib
    Unfortunately for this quick implementation, I had to turn off the
    shading that showed mean+/-std. Now there's just a thin line at these
    locations.

--- app/lag_plot_window.py
+++ app/lag_plot_window.py
@@ -9,6 +9,8 @@ class LagPlotCanvas(FigureCanvasGTK3Agg):
         super(LagPlotCanvas,self).__init__(self.fig)
 
         self.ax = self.fig.add_subplot(1,1,1)
+        self.did_once = False
+        self.vline = None
 
     def new_data(self,t,accum):
         m0 = np.mean( accum, axis=0 )
@@ -18,13 +20,31 @@ class LagPlotCanvas(FigureCanvasGTK3Agg):
             # not enough data yet?
             return {}
 
-        self.ax.cla()
-        self.ax.plot( t, m0, 'b-', lw=2 )
-        self.ax.fill_between( t, m0-s0, m0+s0,
-                              facecolor='b',
-                              alpha=0.4,
-                              linewidth=0 )
+        if self.did_once:
+            self.line.set_xdata(t)
+            self.line.set_ydata(m0)
+
+            self.line_low.set_xdata(t)
+            self.line_low.set_ydata(m0-s0)
+
+            self.line_high.set_xdata(t)
+            self.line_high.set_ydata(m0+s0)
+
+            self.ax.relim()
+            self.ax.autoscale_view(False, False, True)
+        else:
+            self.line, = self.ax.plot( t, m0, 'b-', lw=2 )
+            # self.poly_collection = self.ax.fill_between( t, m0-s0, m0+s0,
+            #                                              facecolor='b',
+            #                                              alpha=0.4,
+            #                                              linewidth=0 )
+            self.line_low,  = self.ax.plot(t, m0-s0, 'b-', alpha=0.4 )
+            self.line_high, = self.ax.plot(t, m0+s0, 'b-', alpha=0.4 )
+            self.did_once = True
         return {'mean':m0}
 
     def show_lag(self, t_crossover):
-        self.ax.axvline( t_crossover )
+        if self.vline is None:
+            self.vline = self.ax.axvline( t_crossover, color='k')
+        else:
+            self.vline.set_xdata([t_crossover])
